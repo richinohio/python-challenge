1c680f30bb22373e5a476fcb75e082f46250c5a6
Richard Marshall <richinohio@gmail.com>
2018-11-17 13:14:57 -0500

    -1st commit

---
+++ Homework 16-D3/assets/js/app.js
@@ -0,0 +1,85 @@
+// @TODO: YOUR CODE HERE!
+//Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. You'll code this graphic in the `app.js` file of your homework directory—make sure you pull in the data from `data.csv` by using the `d3.csv` function. Your scatter plot should ultimately appear like the image at the top of this section.
+
+// Include state abbreviations in the circles.
+
+// Create and situate your axes and labels to the left and bottom of the chart.
+
+	//do stuff with data here 
+//}):
+// data is gone at this point
+
+// same code as above except with the error param
+//d3.csv("assets/data/data.csv").then(function(data, error) {
+	//do stuff with data here 
+//}):
+// data is gone at this point
+
+d3.csv("assets/data/data.csv").then(function(data) {
+     
+    console.log(data);
+    
+    var state = data.map(data => data.state);
+  console.log("States", state);
+
+  // Cast each hours value in tvData as a number using the unary + operator
+  data.forEach(function(data) {
+    data.poverty= +data.poverty;
+    data.healthcare= +data.healthcare;
+    
+    console.log("Poverty:", data.poverty);
+    console.log("No Health Insurance:", data.healthcare);
+  });
+    // Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group area, then set its margins
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  var xScale = d3.scaleLinear()
+  .range([0, chartWidth])
+  .domain(d3.extent(data, data => data.poverty));
+
+// Configure a linear scale with a range between the chartHeight and 0
+// Set the domain for the xLinearScale function
+    var yScale = d3.scaleLinear()
+  .range([chartHeight, 0])
+  .domain([0, d3.max(data, data => data.healthcare)]);
+
+  var bottomAxis = d3.axisBottom(xScale);
+  var leftAxis = d3.axisLeft(yScale);
+
+  chartGroup.append("g")
+  .classed("axis", true)
+  .call(leftAxis);
+
+  chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", "translate(0, " + chartHeight + ")")
+    .call(bottomAxis);
+
+
+    
+  });
+
+
+
